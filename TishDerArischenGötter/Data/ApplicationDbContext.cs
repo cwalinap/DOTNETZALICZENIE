3f3030313c923d23fe18a09aaebbdb146b1fe33f
ptrrk1337 <mymyry1999gmail.com>
2022-06-05 22:12:31 +0200

    Init niesamowity projekt

---
+++ TishDerArischenGötter/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TishDerArischenGötter.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
